bb9ea49ec5f058f3c54cc3db4b02d6618559cda8
username <MY_NAME@example.com>
2023-05-02 12:33:12 +0300

    zoz

--- resources/views/site/layout/layout.blade.php
+++ resources/views/site/layout/layout.blade.php
@@ -10,8 +10,8 @@
     <link rel="stylesheet" href="{{asset('site')}}css/supp.css">
     <link rel="preconnect" href="{{asset('site')}}https://fonts.googleapis.com">
     <link rel="preconnect" href="{{asset('site')}}https://fonts.gstatic.com" crossorigin>
-    <link href="{{asset('site')}}https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&family=Work+Sans:wght@200;300;400;500;600;700;800&display=swap" rel="stylesheet">
-    <link href="{{asset('site')}}https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&family=Work+Sans:wght@200;300;400;500;600;700;800&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 </head>
 
 <body>
