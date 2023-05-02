e47dd7d305f13fa7c7f4015a831ec2ea6e8c4eb7
username <MY_NAME@example.com>
2023-05-02 12:32:50 +0300

    zoz

--- resources/views/site/index.blade.php
+++ resources/views/site/index.blade.php
@@ -1,13 +1,14 @@
 @extends('site.layout.layout')
 @section('body')
 
+
 <body>
     <!-- start header -->
-    <header class="p-3 bg-white text-black fixed-top">
+    <header class="p-3 bg-white text-black sticky-top">
     <div class="container">
         <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
         </a>
-        <img src="public\site\assets\images\site image\pens-2-1238587.jpg" alt="" class="rounded-circle px-2" style="width: 50px;" >
+        <img src="{{asset('site')}}images/logo.png" alt="" class="rounded-circle px-2" style="width: 50px;" >
         <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
             <li><a href="#" class="nav-link px-2 text-black link-warning " onclick="document.location='index.html'">Home</a></li>
             <li><a href="#services" class="nav-link px-2 text-black link-warning">Services</a></li>
@@ -54,7 +55,7 @@
         </form>
 
         <div class="text-end">
-            <button type="button" class="btn btn-outline-light me-2" onclick="document.location='login.html'">Login</button>
+            <button type="button" class="btn btn-outline-dark me-2" onclick="document.location='login.html'">Login</button>
             <button type="button" class="btn btn-warning" onclick="document.location='Sign-up.html'">Sign-up</button>
         </div>
         </div>
@@ -62,14 +63,14 @@
     </header>
     <!-- end header -->
     <!-- start landing -->
-    <div class="landing"> 
-        <div class="overlay"></div> 
+    <div class="landing">
+        <div class="overlay"></div>
             <div class="text">
                 <div class="content">
                     <h2>Hello ! <br> Get Ready to Have Such A Unique Experience With Us </h2>
                     <p>We have got your back. Here you will find all what you need from supplies. You don’t have to pay for useless supplies so you’re able to customize your package and you get only what you want . You can also get your child’s school
-                        supplies and choose their favorites without rolling around for a long time . 
-                        All you have are in one place . 
+                        supplies and choose their favorites without rolling around for a long time .
+                        All you have are in one place .
                         </p>
                 </div>
             </div>
@@ -79,7 +80,7 @@
 <div class="services" id="services">
 <div class="container">
     <h2 class="special-heading"> Sevices </h2>
-    <p> Don't be busy, be productive daviddddddddddddddd</p>
+    <p> Don't be busy, be productive </p>
     <div class="service-content">
     <!--first column-->
     <div class="col">
@@ -122,10 +123,10 @@
                 </div>
             </div> -->
     </div>
-    <!-- third column-->       
+    <!-- third column-->
             <div class="col">
             <div class="image image-col">
-                <img src="public\site\assets\images\site image\pexels-cottonbro-studio-3951862.jpg" alt="">
+                <img src="{{asset('site')}}images/photo_2023-03-11_18-02-09.jpg" alt="">
             </div>
             </div>
         <!-- end services-->
@@ -135,6 +136,95 @@
 </div>
 </div>
 <!--end sevices section-->
+
+<!-- start products -->
+<div class="products" id="products">
+    <h1 class="special-heading">products</h1>
+    <p>get ready to create</p>
+    <div class="section-1">
+        <h1> </h1>
+    <div class="container">
+        <div class="box">
+            <img src="{{asset('site')}}images/Chameleon Deluxe Marker Set.jpg" alt="">
+            <div class="content">
+                <h3> Test title</h3>
+                <p>price:</p>
+            </div>
+            <div class="info">
+                <a href="checkout.html"> Add to cart </a>
+                <i class="fas fa-long-arrow-alt-right"></i>
+            </div>
+        </div>
+        <div class="box">
+            <img src="{{asset('site')}}images/Canson Bloc Aqu.Mont.300g.jpg" alt="">
+            <div class="content">
+                <h3> Test title</h3>
+                <p>price:</p>
+            </div>
+            <div class="info">
+                <a href="checkout.html"> Add to cart </a>
+                <i class="fas fa-long-arrow-alt-right"></i>
+            </div>
+        </div>
+        <div class="box">
+            <img src="images/0720390009000.jpg" alt="">
+            <div class="content">
+                <h3> Test title</h3>
+                <p>price:</p>
+            </div>
+            <div class="info">
+                <a href="checkout.html"> Add to cart </a>
+                <i class="fas fa-long-arrow-alt-right"></i>
+            </div>
+        </div>
+        <div class="box">
+            <img src="images/pencil-new.jpg" alt="">
+            <div class="content">
+                <h3> Faber-Castell pencil </h3>
+                <p>price:</p>
+            </div>
+            <div class="info">
+                <a href="checkout.html"> Add to cart </a>
+                <i class="fas fa-long-arrow-alt-right"></i>
+            </div>
+        </div>
+        <div class="box">
+            <img src="images/cat-05.jpg" alt="">
+            <div class="content">
+                <h3> Test title</h3>
+                <p>price:</p>
+            </div>
+            <div class="info">
+                <a href="checkout.html"> Add to cart </a>
+                <i class="fas fa-long-arrow-alt-right"></i>
+            </div>
+        </div>
+        <div class="box">
+            <img src="images/cat-06.jpg" alt="">
+            <div class="content">
+                <h3> Test title</h3>
+                <p>price:</p>
+            </div>
+            <div class="info">
+                <a href="checkout.html"> Add to cart </a>
+                <i class="fas fa-long-arrow-alt-right"></i>
+            </div>
+        </div>
+        <div class="box">
+            <img src="images/cat-07.jpg" alt="">
+            <div class="content">
+                <h3> Test title</h3>
+                <p>price:</p>
+            </div>
+            <div class="info">
+                <a href="checkout.html"> Add to cart </a>
+                <i class="fas fa-long-arrow-alt-right"></i>
+            </div>
+        </div>
+    </div>
+    </div>
+</div>
+
 <!-- start about section -->
 <div class="about" id="about">
     <div class="container">
@@ -142,7 +232,7 @@
         <p> Less is more work </p>
         <div class="about-content">
         <div class="image">
-            <img src="assets/images/dashboard/user3.jpg" alt="">
+            <img src="images/about.jpg" alt="">
         </div>
         <div class="text">
             <p>Suppmart is a startup company that addresses the problems that all parents have; which is the school supplies shenanigans that take place at the beginning of every academic year. The company started as a group of friends that have younger siblings that are still in school; sharing the problems that they are facing in collecting all the supplies before school starts. They threw a few ideas in order to see if it leads them anywhere useful; and after months of brainstorming and seeing if they can actually achieve the idea they dreamt about, they came up with a solution that would help parents now and the parents to be.</p>
@@ -153,11 +243,12 @@
         </div>
     </div>
     </div>
+
     <!-- start contact-->
     <div class="contact" id="contact">
     <div class="container">
     <h2 class="special-heading">
-        Contact 
+        Contact
     </h2>
     <p>We are born to create</p>
     <div class="content">
@@ -184,9 +275,9 @@
    <div class="footer">
     <div class="container">
     &copy;2023 <span> SuppMart </span> All Right Reserved
-   
+
     <div class="social">
-        Find Us On Social Networks
+        Find Us On Social Networks <br>
         <i class="fa-brands fa-youtube"></i>
         <i class="fa-brands fa-facebook-f"></i>
         <i class="fa-brands fa-twitter"></i>
@@ -194,6 +285,10 @@
       </div>
    </div>
    <!-- end footer -->
-    {{-- <script src="js/bootstrap.bundle.min.js"></script>
-    <script src="js/all.min.js"></script> --}}
+    <script src="js/bootstrap.bundle.min.js"></script>
+    <script src="js/all.min.js"></script>
+</body>
+</html>
+
+
 </body>
--- resources/views/site/layout/layout.blade.php
+++ resources/views/site/layout/layout.blade.php
@@ -1,95 +1,85 @@
 
 <!DOCTYPE html>
 <html lang="en"><head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width,initial-scale=1">
-    <meta name="description" content="multikart">
-    <meta name="keywords" content="multikart">
-    <meta name="author" content="multikart">
-    <link rel="icon" href="../assets/images/favicon/1.png" type="image/x-icon">
-    <link rel="shortcut icon" href="../assets/images/favicon/1.png" type="image/x-icon">
-    <title>Multikart - Multi-purpose E-commerce Html Template</title>
-   
-    <!--Google font-->
-    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900" rel="stylesheet">
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Yellowtail&amp;display=swap" rel="stylesheet">
-
-    <!-- Icons -->
-    <link rel="stylesheet" type="text/css" href="{{asset('site')}}/assets/css/vendors/font-awesome.css">
-  
-    <!--Slick slider css-->
-    <link rel="stylesheet" type="text/css" href="{{asset('site')}}/assets/css/vendors/slick.css">
-    <link rel="stylesheet" type="text/css" href="{{asset('site')}}/assets/css/vendors/slick-theme.css">
-
-    <!-- Animate icon -->
-    <link rel="stylesheet" type="text/css" href="{{asset('site')}}/assets/css/vendors/animate.css">
-
-    <!-- Themify icon -->
-    <link rel="stylesheet" type="text/css" href="{{asset('site')}}/assets/css/vendors/themify-icons.css">
-
-    <!-- Bootstrap css -->
-    <link rel="stylesheet" type="text/css" href="{{asset('site')}}/assets/css/vendors/bootstrap.css">
-
-    <!-- Theme css -->
-    <link rel="stylesheet" type="text/css" href="{{asset('site')}}/assets/css/style.css">
-
-
-
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SuppMart</title>
+    <link rel="stylesheet" href="{{asset('site')}}css/bootstrap.min.css">
+    <link rel="stylesheet" href="{{asset('site')}}css/all.min.css">
+    <link rel="stylesheet" href="{{asset('site')}}css/supp.css">
+    <link rel="preconnect" href="{{asset('site')}}https://fonts.googleapis.com">
+    <link rel="preconnect" href="{{asset('site')}}https://fonts.gstatic.com" crossorigin>
+    <link href="{{asset('site')}}https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&family=Work+Sans:wght@200;300;400;500;600;700;800&display=swap" rel="stylesheet">
+    <link href="{{asset('site')}}https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 </head>
 
-<body class="theme-color-1" style="overflow: auto;">
+<body>
 
+     <!-- start header -->
+     <header class="p-3 bg-white text-black sticky-top">
+        <div class="container">
+            <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+            </a>
+            <img src="{{asset('site')}}images/logo.png" alt="" class="rounded-circle px-2" style="width: 50px;" >
+            <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+                <li><a href="#" class="nav-link px-2 text-black link-warning " onclick="document.location='index.html'">Home</a></li>
+                <li><a href="#services" class="nav-link px-2 text-black link-warning">Services</a></li>
+                <li><a href="#" class="nav-link px-2 text-black link-warning" onclick="document.location='products.html'">Products</a></li>
+                <li><a href="#" class="nav-link px-2 text-black link-warning dropdown-toggle btn" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Schools</a>
+                    <ul class="dropdown-menu dropdown-menu-dark">
+                        <li><a class="dropdown-item active" href="#">cairo american college</a></li>
+                        <li><a class="dropdown-item" href="#">British Internationa</a></li>
+                        <li><a class="dropdown-item" href="#">Lycée la Liberté</a></li>
+                        <li><hr class="dropdown-divider"></li>
+                        <li><a class="dropdown-item" href="#">Separated link</a></li>
+                    </ul>
+                </li>
+                <li><a href="#" class="nav-link px-2 text-black link-warning dropdown-toggle btn" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Brands</a>
+                    <ul class="dropdown-menu dropdown-menu-dark">
+                        <li><a class="dropdown-item active" href="#">Faber-Castell</a></li>
+                        <li><a class="dropdown-item" href="#">YOKEN</a></li>
+                        <li><a class="dropdown-item" href="#">ROTRING</a></li>
+                        <li><hr class="dropdown-divider"></li>
+                        <li><a class="dropdown-item" href="#">Separated link</a></li>
+                    </ul>
+                </li>
+                <li><a href="#" class="nav-link px-2 text-black link-warning dropdown-toggle btn" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">categories</a>
+                    <ul class="dropdown-menu dropdown-menu-dark">
+                        <li><a class="dropdown-item active" href="#">pens $ pencils</a></li>
+                        <li><a class="dropdown-item" href="#">notebooks</a></li>
+                        <li><a class="dropdown-item" href="#">bags</a></li>
+                        <li><hr class="dropdown-divider"></li>
+                        <li><a class="dropdown-item" href="#">Separated link</a></li>
+                    </ul>
+                </li>
+                <li><a href="#contact" class="nav-link px-2 text-black link-warning">Contact Us</a></li>
+                <li><a href="#about" class="nav-link px-2 text-black link-warning">About</a></li>
+            </ul>
+            <ul class="dropdown-menu dropdown-menu-dark">
+                <li><a class="dropdown-item active" href="#">Action</a></li>
+                <li><a class="dropdown-item" href="#">Another action</a></li>
+                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                <li><hr class="dropdown-divider"></li>
+                <li><a class="dropdown-item" href="#">Separated link</a></li>
+            </ul>
+            <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
+                <input type="search" class="form-control form-control-dark" placeholder="Search..." aria-label="Search">
+            </form>
 
-   
-    <div class="tap-top top-cls" style="display: none;">
-        <div>
-            <i class="fa fa-angle-double-up"></i>
-            <h1>daviddd
-
-            </h1>
+            <div class="text-end">
+                <button type="button" class="btn btn-outline-dark me-2" onclick="document.location='login.html'">Login</button>
+                <button type="button" class="btn btn-warning" onclick="document.location='Sign-up.html'">Sign-up</button>
+            </div>
+            </div>
         </div>
-    </div>
-    <!-- tap to top end -->
+        </header>
+        <!-- end header -->
 
 
-    <!-- latest jquery-->
-    <script src="{{asset('site')}}/assets/js/jquery-3.3.1.min.js"></script>
-
-    <!-- fly cart ui jquery-->
-    {{-- <script src="{{asset('site')}}/assets/js/jquery-ui.min.js"></script> --}}
-
-    <!-- exitintent jquery-->
-    <script src="{{asset('site')}}/assets/js/jquery.exitintent.js"></script>
-    {{-- <script src="{{asset('site')}}/assets/js/exit.js"></script> --}}
-
-    <!-- slick js-->
-    <script src="{{asset('site')}}/assets/js/slick.js"></script>
-
-    <!-- menu js-->
-    <script src="{{asset('site')}}/assets/js/menu.js"></script>
-
-    <!-- lazyload js-->
-    <script src="{{asset('site')}}/assets/js/lazysizes.min.js"></script>
-
-    <!-- Bootstrap js-->
-    <script src="{{asset('site')}}/assets/js/bootstrap.bundle.min.js"></script>
-
-    <!-- Bootstrap Notification js-->
-    {{-- <script src="{{asset('site')}}/assets/js/bootstrap-notify.min.js"></script> --}}
-
-    <!-- Fly cart js-->
-    {{-- <script src="{{  asset('site') }}/assets/js/fly-cart.js"></script> --}}
-
-    <!-- Theme js-->
-    
-    {{-- <script src="{{asset('site')}}/assets/js/theme-setting.js"></script> --}}
-    
-    {{-- <script src="{{asset('site')}}/assets/js/script.js"></script> --}}
     <script src="{{asset('site')}}/js/bootstrap.bundle.min.js"></script>
     <script src="{{asset('site')}}/js/all.min.js"></script>
-<h1>zeinab </h1>
+
     <script>
         $(window).on('load', function () {
             setTimeout(function () {
